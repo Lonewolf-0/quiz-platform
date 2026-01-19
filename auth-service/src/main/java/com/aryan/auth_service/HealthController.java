d2b6f40f7faada76fc2d0ef187b5c88aaa88dbf4
lonewolf-0 <aryan.tyagi202@gmail.com>
2026-01-20 02:16:06 +0530

    add initial implementation of auth service with health check endpoint

---
+++ auth-service/src/main/java/com/aryan/auth_service/HealthController.java
@@ -0,0 +1,16 @@
+package com.aryan.auth_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "AUTH SERVICE OK";
+    }
+}
+
