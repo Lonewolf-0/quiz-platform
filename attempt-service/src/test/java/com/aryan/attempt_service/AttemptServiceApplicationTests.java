37f57bb1994a90aa9afb628c189752ca2ba1cbdf
lonewolf-0 <aryan.tyagi202@gmail.com>
2026-01-20 02:20:25 +0530

    add initial implementation of quiz service with Maven setup

---
+++ attempt-service/src/test/java/com/aryan/attempt_service/AttemptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.attempt_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttemptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
