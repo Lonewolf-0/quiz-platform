e49239f34fe0f41b21f435b3192f020651b0d1f7
lonewolf-0 <aryan.tyagi202@gmail.com>
2026-01-19 23:48:12 +0530

    initialize discovery and gateway services with Spring Boot and Maven setup

---
+++ discovery-service/src/test/java/com/aryan/discovery_service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
